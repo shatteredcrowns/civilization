1260660bd8d40cd8181b869d3f1dd7062f59e1b7
shatteredcrowns <pj731764@gmail.com>
2025-05-09 16:54:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
 // Function to send form data to Discord webhook
 function sendToDiscord(discordName, inGameTag, civilization, referralCode) {
-    const webhookUrl = "https://discord.com/api/webhooks/1370312491212476451/PHBiLu5yespJgkBwl53O_dyTKWRudctxbCgMWTfhR-JC4Yc_Uo0bEdW_DHwYcQmIQEQ9"; // Your Discord Webhook URL
+    const webhookUrl = "https://discord.com/api/webhooks/1370360515708059689/YAXWUGnucISjF--yqqlF2pU09q0_n9uGojnJe73tAR4MXeUjiUfzRobM3HSwL5zKyV9Z"; // Your Discord Webhook URL
     
     const payload = {
         content: `**New Contact Form Submission**\n**Discord Name:** ${discordName}\n**In-Game Tag:** ${inGameTag}\n**Civilization:** ${civilization}\n**Referral Code:** ${referralCode}`,
